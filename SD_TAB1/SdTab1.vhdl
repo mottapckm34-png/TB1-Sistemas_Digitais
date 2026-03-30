f2685bcf847e5f22542a288cf9a917bce2f0f874
alexll03 <alexliuBr2@outlook.com>
2026-03-29 21:30:31 -0300

    ComeitAlex

--- SD_TAB1/SdTab1.vhdl
+++ SD_TAB1/SdTab1.vhdl
@@ -33,19 +33,32 @@ architecture Behavioral of ULA is
 process(a, b, op)
     variable ext_a              : unsigned (4 downto 0); -- Turn 4 bit into 5 bit
     variable ext_b              : unsigned (4 downto 0); -- Turn 4 bit into 5 bit
-    variable temporaryResult    : unsigned (4 downto 0); -- Turn 4 bit into 5 bit
+    variable tempResult         : unsigned (4 downto 0); -- Turn 4 bit into 5 bit
 
 begin
-    ext_a           := unsigned ('0' & a); -- Concatenarion, places a '0' in front of every bit
-    ext_b           := unsigned ('0' & a); -- Concatenarion, places a '0' in front of every bit
-    temporaryResult := unsigned (others => '0'); -- Take note of the result before send to the LEDS, "(others => '0')" this part will turn every bit in 0. This part of the code serves to avoid unwanted memory, trash;
+    ext_a      := unsigned ('0' & a);       -- Concatenarion, places a '0' in front of every bit
+    ext_b      := unsigned ('0' & a);       -- Concatenarion, places a '0' in front of every bit
+    tempResult := unsigned (others => '0'); -- Take note of the result before send to the LEDS, "(others => '0')" this part will turn every bit in 0. This part of the code serves to avoid unwanted memory, trash;
 
     case op is
-        --Entra as operações
+
+        -- Soma a complemento de 2
+        when "000" => tempResult := ext_a + ext_b;   
+
+        -- Subtração a complemento de 2, COMPLEMENTO A 2: A + (-B) + 1, to_unsigned (1, 5) representa a soma +1, gera um valor número 1 de tamanho de 5 bits
+        when "001" => tempResult := ext_a + (NOT ext_b) + to_unsigned (1, 5); 
+        
+        -- AND, '0' & Faz a concatenação obrigando a ter 5 bits, (unsigned(a) AND unsigned(b)) compara bit a bit, bit 0 de A com o bit 0 de B
+        when "010" => tempResult := '0' & (unsigned(a) AND unsigned(b)); 
+
+        -- OR, '0' & Faz a concatenação obrigando a ter 5 bits, (unsigned(a) AND unsigned(b)) compara bit a bit, bit 0 de A com o bit 0 de B
+        when "011" => tempResult := '0' & (unsigned(a) OR unsigned(b));
+
     end case;
 
-    rest5 <= std_logic_vector(temporaryResult); -- Recive 
+    rest5 <= std_logic_vector(tempResult); -- Act as a output signal, electric signal
 
+end process;
 
 
 -- FMS - CONTROLE DE MEMÓRIA  
